10d4045ec578b017a6a60a4be51540b5f4a0f7fb
Tianhao <zhangth1990@hotmail.com>
2018-06-25 18:25:48 -0600

    set up login screen with login and signUp

--- app/src/main/java/com/example/tianhao/chatapporeo/MainActivity.kt
+++ app/src/main/java/com/example/tianhao/chatapporeo/MainActivity.kt
@@ -2,11 +2,84 @@ package com.example.tianhao.chatapporeo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.AuthResult
+import com.google.android.gms.tasks.Task
+import android.support.annotation.NonNull
+import com.google.android.gms.tasks.OnCompleteListener
+import android.R.attr.password
+import android.content.Intent
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import android.R.attr.password
+import android.R.attr.password
+//import com.google.firebase.database.FirebaseDatabase
+
 
 class MainActivity : AppCompatActivity() {
+    lateinit var emailEditText: EditText
+    lateinit var passwordEditText: EditText
+    val mAuth = FirebaseAuth.getInstance()
+    var email = ""
+    var password = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        emailEditText = findViewById(R.id.emailEditText) as EditText
+        passwordEditText = findViewById(R.id.passwordEditText) as EditText
+
+        if (mAuth.currentUser != null) {
+            logIn()
+        }
+    }
+    fun loginButton(view: View) {
+
+        //Toast.makeText(this, "email: " + emailEditText.text.toString() + " password:" +passwordEditText.text.toString(), Toast.LENGTH_LONG).show()
+        if (! emailEditText.text.toString().isEmpty() && !emailEditText.text.toString().isEmpty()) {
+            mAuth.signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(this, "signInWithEmail:success", Toast.LENGTH_LONG).show()
+                            logIn()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "Authentication failed", Toast.LENGTH_LONG).show()
+                        }
+                    }
+        }else{
+            Toast.makeText(this, "Please fill up the credentials :(", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun goClick(view: View){
+        //Check if we can log in the user
+        if (! emailEditText.text.toString().isEmpty() && !emailEditText.text.toString().isEmpty()) {
+            mAuth.createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
+                    .addOnCompleteListener(this) { task ->
+                        if (task.isSuccessful) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(this, "createUserWithEmail:success", Toast.LENGTH_LONG).show()
+                            //FirebaseDatabase.getInstance().getReference().child("users").child(task.result.user.uid).child("email").setValue(emailEditText.text.toString())
+                            logIn()
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(this, "user already existed or wrong format", Toast.LENGTH_LONG).show()
+                        }
+                    }
+        }else{
+            Toast.makeText(this, "Please fill up the credentials :(", Toast.LENGTH_LONG).show()
+        }
+    }
+    fun logIn(){
+        //Move to next Activity
+        val intent = Intent(this, SnapsActivity::class.java)
+        startActivity(intent)
+
     }
 }
